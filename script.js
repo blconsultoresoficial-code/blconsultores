5296bc19777fcefe65668166949a56761c597ddf
blconsultoresoficial-code <blconsultoresoficial@gmail.com>
2026-05-01 16:29:25 -0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function mostrarSeccion(id) {
-    const secciones = document.querySelectorAll('.seccion');
-
-    secciones.forEach(sec => {
-        sec.classList.remove('activa');
-    });
-
-    document.getElementById(id).classList.add('activa');
-}
